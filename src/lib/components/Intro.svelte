6c815139739913f7d87f889b8dfd5df9731bc951
Paul Vezzetti <48961730+paulvezzetti@users.noreply.github.com>
2023-12-28 15:50:29 -0500

    Updates to Intro

--- src/lib/components/Intro.svelte
+++ src/lib/components/Intro.svelte
@@ -1,28 +1,72 @@
 <script lang="ts"></script>
 
 <section class="intro">
-    <h1>Hi there! I'm Paul Vezzetti</h1>
+	<div class="main-block">
+		<p class="subtle">Hello! My name is</p>
+		<p class="attention">Paul Vezzetti</p>
+	</div>
+    <div class="detail">
+        <div class="left-bar"></div>
+        <p class="long-text">
+            I am a Front-End software engineer specializing in Data Visualization for Business
+            Intelligence and Data Exploration applications.
+        </p>
+    </div>
 </section>
-
 
 <style>
 	section {
-        background-color: #2c2c2c;
+		background-color: #2c2c2c;
 		display: flex;
-		flex-direction: row;
-		justify-content: center;
-		align-items: center;
-        height: 100vh;
+        flex-direction: column;
+		height: 100vh;
 	}
 
-	h1 {
+	p {
 		width: 100%;
-        color: #d3d3d3;
+		color: #d3d3d3;
+		margin: 0;
 	}
 
-    .intro {
-        background-image: url('../images/abstract-background.png');
-        background-size: cover;
+	.intro {
+		background-image: url('../images/abstract-background.png');
+		background-size: cover;
+	}
+
+	.main-block {
+		margin-left: 10vw;
+        margin-top: 5vh;
+		display: flex;
+		flex-direction: column;
+	}
+
+	.subtle {
+        font-family: 'Courier New', Courier, monospace;
+		font-size: 1.6vw;
+	}
+
+	.attention {
+		font-size: 10vw;
+	}
+
+    .detail {
+        display: flex;
+        column-gap: 10px;
+        position: absolute;
+        left: 10vw;
+        bottom: 10vh;
     }
 
+	.left-bar {
+        width: 4px;
+        background: linear-gradient(to bottom, #a09cd0, #0831ff);
+	}
+
+	.long-text {
+		font-size: 2vw;
+		color: #f3f3f3;
+        max-width: 60vw;
+        letter-spacing: 1.5px;
+
+	}
 </style>
