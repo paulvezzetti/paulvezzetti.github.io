6adaba7c39d7f30af29ede6fe69e86fddfdb32aa
Paul Vezzetti <48961730+paulvezzetti@users.noreply.github.com>
2023-12-28 13:16:44 -0500

    Beginning of Intro section

---
+++ src/lib/components/Intro.svelte
@@ -0,0 +1,28 @@
+<script lang="ts"></script>
+
+<section class="intro">
+    <h1>Hi there! I'm Paul Vezzetti</h1>
+</section>
+
+
+<style>
+	section {
+        background-color: #2c2c2c;
+		display: flex;
+		flex-direction: row;
+		justify-content: center;
+		align-items: center;
+        height: 100vh;
+	}
+
+	h1 {
+		width: 100%;
+        color: #d3d3d3;
+	}
+
+    .intro {
+        background-image: url('../images/abstract-background.png');
+        background-size: cover;
+    }
+
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,48 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+    import Intro from "$lib/components/Intro.svelte";
+</script>
+
+<svelte:head>
+	<title>Paul Vezzetti</title>
+	<meta name="description" content="Personal Page for Paul Vezzetti" />
+</svelte:head>
+
+
+<Intro></Intro>
+<!-- <section class="intro">
+    <h1>Intro</h1>
+</section> -->
+<section>
+    <h1>About</h1>
+</section>
+<section>
+    <h1>Skills</h1>
+</section>
+<section>
+    <h1>Experience</h1>
+</section>
+<section>
+    <h1>Projects</h1>
+</section>
+<section>
+    <h1>Contact</h1>
+</section>
+
+
+<style>
+	section {
+        background-color: #2c2c2c;
+		display: flex;
+		flex-direction: row;
+		justify-content: center;
+		align-items: center;
+        height: 100vh;
+	}
+
+	h1 {
+		width: 100%;
+        color: #d3d3d3;
+	}
+
+</style>
+
